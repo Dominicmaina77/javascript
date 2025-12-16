d56f7c6ab23878b3ece941324c350b0a2ddf07b5
Dominicmaina77 <mainadominic628@gmail.com>
2025-12-16 11:58:08 +0300

    finishedfunctions

--- js/function.js
+++ js/function.js
@@ -17,14 +17,14 @@ document.getElementById("demo").innerHTML = sum;
 //     return (5/9) * (f-32);
 // }
 const myCalc = (y) => {
-return  (5 / 9) * (y - 32);
+  return (5 / 9) * (y - 32);
 };
 
 let sum1 = myCalc(100);
 document.getElementById("value").innerHTML = sum1;
 
-let mySum = (a,b) => a * b;
-let sum2 = mySum(5,6);
+let mySum = (a, b) => a * b;
+let sum2 = mySum(5, 6);
 document.getElementById("data").innerHTML = sum2;
 
 // function as an event
@@ -38,18 +38,77 @@ function displayDate() {
 // this refers to how js handles tasks that take time to complete
 // eg, loading data from a server or waiting for a user to respond or reading files
 // js uses asynchronus which allows operations to run in the background and their results handled later
-// asynchronus patterns 
+// asynchronus patterns
 // events , callbacks, promises, async/await
 
 // callback function
 // a function that is passed as an argument to another function and is executed after the other function has completed
- function myDisplayer(some) {
-    document.getElementById("type").innerHTML = some;
-  }
+function myDisplayer(some) {
+  document.getElementById("type").innerHTML = some;
+}
+
+function myCalculator(num1, num2, myCallback) {
+  let sum = num1 + num2;
+  myCallback(sum);
+}
+
+myCalculator(5, 5, myDisplayer);
+
+// js promises
+// a promise is an object that represents the eventual completion or failure of an asynchronous operation and its resulting value
+//states of a promise
+// pending: initial state, neither fulfilled nor rejected
+// fulfilled: meaning that the operation was completed successfully
+// rejected: meaning that the operation failed
+// synatx
+//const myPromise = new Promise(function(resolve, reject) {
+// do an async task, such as fetch data from a server/API/file read
+//  if(success){
+// resolve(value);
+// }else{
+// reject(error);
+// }
+//
+// });
+
+
+// using then and catch
+// myPromise
+//   .then((result)=>{
+//     console.log(result); //success
+//   })
+//   .catch((error)=>{
+//     console.error(error);
+//   })
   
-  function myCalculator(num1, num2, myCallback) {
-    let sum = num1 + num2;
-    myCallback(sum);
+  // eg : fetching data
+  function fetchData(){
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        const success = true;
+        if(success){
+          resolve({"name":"John","age":30});
+        }else{
+          reject(new Error("Error fetching data"));
+        }
+      }, 1000);
+    })
   }
-  
-  myCalculator(5, 5, myDisplayer);
+  fetchData()
+  .then(response => console.log(response))
+  .catch(error => console.error(error.message));
+
+  // async/ await
+  // async : declares the function as asynchronous
+  // await : waits for a promise to resolve and returns the resolved value(fulfilled , rejected)
+  // await can only be used inside an async function
+  const processUserData= async ()=>{
+     try {
+      const response = await fetchData();
+      console.log(response);
+     } catch (error) {
+      console.error(error.message);
+     }
+  } 
+  processUserData();
+
